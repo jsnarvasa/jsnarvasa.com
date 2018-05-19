0cda08f26ed8fdb39a4706f5426cbdcc2ae4a703
jsnarvasa <jsnarvasa@users.noreply.github.com>
2018-05-20 09:25:46 +1000

    Updated index.html and main.py to display photo gallery

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+from os import listdir
 
 # the all-important app variable:
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template("index.html")
+def index():
+    image_names = listdir('./static/images')
+    return render_template("index.html", image_names=image_names)
+
+@app.route("/send_image/<filename>")
+def send_image(filename):
+    return send_from_directory("static/images", filename)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
