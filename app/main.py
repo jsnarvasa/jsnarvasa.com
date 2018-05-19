551b2f2b109850e4beffbcbcb28856c483e65c7b
jsnarvasa <jsnarvasa@users.noreply.github.com>
2018-05-19 21:20:53 +1000

    Added directory structure, added index.html, updated main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # the all-important app variable:
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Oh, Hello World"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
