4235c25be3b19e3d22dac0b967ea18c57d0dfab2
jsnarvasa <jsnarvasa@users.noreply.github.com>
2018-05-27 23:24:39 +1000

    Reinstalled venv, added Flask-MySQLdb + yaml + pylint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template, send_from_directory
 from os import listdir
+from flask_mysqldb import MySQL
+import yaml
 
 # the all-important app variable:
 app = Flask(__name__)
+
+# Configure Database
+db = yaml.load(open('database_config.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+
+mysql = MySQL(app)
 
 @app.route("/")
 def index():
@@ -14,4 +25,5 @@ def send_image(filename):
     return send_from_directory("static/images", filename)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=80)
+    #app.run(host='0.0.0.0', debug=True, port=80)
+    app.run()
