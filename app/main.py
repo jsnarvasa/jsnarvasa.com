daafb09a37101ad37fce4dc1bd94b1cc436cf188
jsnarvasa <jsnarvasa@users.noreply.github.com>
2018-06-09 22:44:19 +1000

    Added dependency on mysqlclient==1.3.12, updated html template files

--- app/main.py
+++ app/main.py
@@ -9,12 +9,10 @@ app = Flask(__name__)
 
 # Configure Database
 db = yaml.load(open('database_config.yaml'))
-
 mysql_user = db["mysql_user"]
 mysql_password = db["mysql_password"]
 mysql_host = db["mysql_host"]
 mysql_db = db["mysql_db"]
-
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://" + mysql_user + ":" + mysql_password + "@" + mysql_host + "/" + mysql_db
 db = SQLAlchemy(app)
 
@@ -33,9 +31,11 @@ def index():
     image_names = Photos.query.all()
     return render_template("index.html", image_names=image_names)
 
+
 @app.route("/send_image/<filename>")
 def send_image(filename):
     return send_from_directory("static/images/", filename)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
