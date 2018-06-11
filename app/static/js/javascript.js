ba69bf1704da65138d94194f5c02b41b40709cf7
jsnarvasa <jsnarvasa@users.noreply.github.com>
2018-06-11 21:10:07 +1000

    Added comment messages for readability

--- app/static/js/javascript.js
+++ app/static/js/javascript.js
@@ -1,3 +1,4 @@
+// To enable main-navbar element transparency when scrolling
 $(document).ready(function() {
 
     var navbar = $('#main-navbar');
@@ -20,7 +21,7 @@ $(document).ready(function() {
 
 });
 
-// To center splash-intro message @index.html
+// To center splash-intro message @index.html and keep centred on window resize
 function splashIntro() {
     var splashImgHeight = $('#splash-image-div').height();
     $('#splash-intro').css('margin-top',splashImgHeight/2.2);
