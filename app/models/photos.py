f0a230c018b76ba9d5255489b8aa2b8436ee19ab
jsnarvasa <jsnarvasa@users.noreply.github.com>
2018-06-17 11:05:11 +1000

    Updated column type photos.py

--- app/models/photos.py
+++ app/models/photos.py
@@ -4,7 +4,7 @@ from main import db
 class Photos(db.Model):
     __tablename__ = 'photos'
     PhotoID = db.Column('PhotoID',db.Integer, primary_key = True)
-    FileName = db.Column('FileName', db.Unicode)
+    FileName = db.Column('FileName', db.String(30))
 
     def __repr__(self):
         return '<Photos %r>' % self.FileName
