4ce6101702bebbf7251df0850db348f6d6baaa77
jsnarvasa <jsnarvasa@users.noreply.github.com>
2018-07-01 20:32:01 +1000

    added photo overlay

--- app/app/static/js/javascript.js
+++ app/app/static/js/javascript.js
@@ -29,3 +29,23 @@ function splashIntro() {
 
 $(document).ready(splashIntro);
 $(window).on('resize', splashIntro);
+
+
+// Display image overlay in Gallery
+
+function windowHeight() {
+    var height = $(window).height();
+    return height;
+}
+
+$(document).ready(windowHeight);
+$(window).on('resize', windowHeight);
+
+$('.gallery-image').on('click', function(){
+    var image = '<img class="img-fluid" style="height:100%;max-width:100%" src="' + $(this).attr('src') + '"></img>';
+    $('#overlay-container').css("visibility", "visible");
+    $('#overlay-image').append(image);
+    $('#overlay-image').css("height", windowHeight()*0.9);
+    //var overlayImgHeight = $('#overlay-image').height();
+    //$('#overlay-container').css("height",overlayImgHeight);
+});
