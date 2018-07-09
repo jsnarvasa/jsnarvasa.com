7219e85f84c5a78b20eafc8c93642e1ec8c635e1
jsnarvasa <jsnarvasa@users.noreply.github.com>
2018-07-09 22:58:10 +1000

    Fixed jQuery bug not waiting for DOM to load

--- app/app/static/js/javascript.js
+++ app/app/static/js/javascript.js
@@ -1,79 +1,83 @@
-// To enable main-navbar element transparency when scrolling
 $(document).ready(function() {
+    
+    // To enable main-navbar element transparency when scrolling
+    $(document).ready(function() {
 
-    var navbar = $('#main-navbar');
-    var container = $('#main-container');
+        var navbar = $('#main-navbar');
+        var container = $('#main-container');
 
-    // The order of adding and removing the classes matters here
-    function scroll() {
-        if ($(window).scrollTop() > 0) {
-            container.addClass('navbar-scroll-padding');
-            navbar.addClass('navbar-opacity');
-            navbar.addClass('fixed-top');
-        } else {
-            navbar.removeClass('fixed-top');
-            navbar.removeClass('navbar-opacity');
-            container.removeClass('navbar-scroll-padding');
-        }
+        // The order of adding and removing the classes matters here
+        function scroll() {
+            if ($(window).scrollTop() > 0) {
+                container.addClass('navbar-scroll-padding');
+                navbar.addClass('navbar-opacity');
+                navbar.addClass('fixed-top');
+            } else {
+                navbar.removeClass('fixed-top');
+                navbar.removeClass('navbar-opacity');
+                container.removeClass('navbar-scroll-padding');
+            }
+        };
+
+        document.onscroll = scroll;
+
+    });
+
+    // To center splash-intro message @index.html and keep centred on window resize
+    function splashIntro() {
+        var splashImgHeight = $('#splash-image-div').height();
+        $('#splash-intro').css('margin-top',splashImgHeight/2.2);
     };
 
-    document.onscroll = scroll;
-
-});
-
-// To center splash-intro message @index.html and keep centred on window resize
-function splashIntro() {
-    var splashImgHeight = $('#splash-image-div').height();
-    $('#splash-intro').css('margin-top',splashImgHeight/2.2);
-};
-
-$(document).ready(splashIntro);
-$(window).on('resize', splashIntro);
+    $(document).ready(splashIntro);
+    $(window).on('resize', splashIntro);
 
 
-// Sets the image overlay height in Gallery
+    // Sets the image overlay height in Gallery
 
-function getWindowHeight() {
-    var windowHeight = $(window).height();
-    return windowHeight;
-};
+    function getWindowHeight() {
+        var windowHeight = $(window).height();
+        return windowHeight;
+    };
 
-function adjustOverlayHeight() {
-    var defaultHeight = getWindowHeight()*0.9;
-    $('#overlay-image').css("height", defaultHeight);
-};
+    function adjustOverlayHeight() {
+        var defaultHeight = getWindowHeight()*0.9;
+        $('#overlay-image').css("height", defaultHeight);
+    };
 
-$('.gallery-image').on('click', adjustOverlayHeight);
-$(window).on('resize',adjustOverlayHeight);
+    $('.gallery-image').on('click', adjustOverlayHeight);
+    $(window).on('resize',adjustOverlayHeight);
 
 
-// Displays and closes the overlay in Gallery
+    // Displays and closes the overlay in Gallery
 
-function overlayOffset() {
-    var scrollOffset = $(window).scrollTop();
-    $("#overlay-container").css("top", scrollOffset + getWindowHeight()*0.05);
-};
+    function overlayOffset() {
+        var scrollOffset = $(window).scrollTop();
+        $("#overlay-container").css("top", scrollOffset + getWindowHeight()*0.05);
+    };
 
-$('.gallery-image').on('click', function(){
-    var image = '<img class="img-fluid" id="image-on-overlay" src="' + $(this).attr('src') + '"></img>';
-    $('#overlay-container').css("visibility", "visible");
-    $('#overlay-container').css("top", overlayOffset());
-    $('#overlay-image').append(image);
-    // AJAX request here
-    $.getJSON('/getphotodetails',{
-        img: $(this).attr('id')
-    }, function(data) {
-        $("#caption").text(data.Caption);
-        $("#city").text(data.City);
-        $("#country").text(data.Country);
-        $("#uploadDate").text(data.Upload_Date);
-        $("#captureDate").text(data.Capture_Date);
+    $('.gallery-image').on('click', function(){
+        var image = '<img class="img-fluid" id="image-on-overlay" src="' + $(this).attr('src') + '"></img>';
+        $('#overlay-container').css("visibility", "visible");
+        $('#overlay-container').css("top", overlayOffset());
+        $('#overlay-image').append(image);
+        // AJAX request here
+        $.getJSON('/getphotodetails',{
+            img: $(this).attr('id')
+        }, function(data) {
+            $("#caption").text(data.Caption);
+            $("#city").text(data.City);
+            $("#country").text(data.Country);
+            $("#uploadDate").text(data.Upload_Date);
+            $("#captureDate").text(data.Capture_Date);
+        });
     });
-});
 
-$('#overlay-close').on('click', function(){
-    $('#overlay-container').css("visibility", "hidden");
-    $('#overlay-image').empty();
-});
+    $('#overlay-close').on('click', function(){
+        $('#overlay-container').css("visibility", "hidden");
+        $('#overlay-image').empty();
+    });
 
-$(window).on('resize', overlayOffset());
+    $(window).on('resize', overlayOffset());
+    
+});
