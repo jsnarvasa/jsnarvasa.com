b8d4a575d2255e4830c72ee3f32d4dbcd6ce0390
jsnarvasa <jsnarvasa@users.noreply.github.com>
2019-10-20 19:10:24 +1100

    Added port for PROD

--- app/run.py
+++ app/run.py
@@ -7,6 +7,6 @@ hostname = socket.gethostname()
 if __name__ == "__main__":
     db.create_all()
     if hostname == config.hostname['PROD']:
-        app.run()
+        app.run(port=6000)
     else:
         app.run(host='localhost', port=80, debug=True)
