57cf72a91f96ce77ee6288b58a9e57c345e72a3d
jsnarvasa <jsnarvasa@users.noreply.github.com>
2019-10-20 18:18:44 +1100

    Added server deployment config

--- app/run.py
+++ app/run.py
@@ -7,6 +7,6 @@ hostname = socket.gethostname()
 if __name__ == "__main__":
     db.create_all()
     if hostname == config.hostname['PROD']:
-        app.run(host='localhost', port=80, debug=False)
+        app.run()
     else:
         app.run(host='localhost', port=80, debug=True)
