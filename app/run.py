21c3b47f5eabeb4e555264c4630d949ee6f9f3ad
jsnarvasa <jsnarvasa@users.noreply.github.com>
2019-12-08 20:43:51 +1100

    Working version; fixed two issues: 1. Database tables do not get automatically created in PROD, however works in DEV.  Issue is due to run.py being referrenced by uwsgi.ini which doesn't execute db.create_all() when inside the if statement. Confirmed by adding logging inside the if statement, which did not get added to log file. 2. Unable to upload file in PROD.  Issue due to token, no code change required.

--- app/run.py
+++ app/run.py
@@ -3,10 +3,10 @@ from app import app, db
 import config
 
 hostname = socket.gethostname()
+db.create_all()
 
 if __name__ == "__main__":
-    db.create_all()
     if hostname == config.hostname['PROD']:
-        app.run(port=6000, debug=True)
+        app.run(port=6000)
     else:
         app.run(host='localhost', port=5000, debug=True)
